3ac921468ac50226a819658fa4ff075618f5f1fd
rouanis5 <ouanis42@gmail.com>
2022-05-14 16:55:05 +0100

    + database file

---
+++ backend/dataBase.sql
@@ -0,0 +1,31 @@
+-- Base de données :  `blog`
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `posts`
+--
+-- @block
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post` text NOT NULL,
+  `date_post` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+--
+-- Structure de la table `comments`
+--
+-- @block 
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `comment` text NOT NULL,
+  `date_comment` date NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
