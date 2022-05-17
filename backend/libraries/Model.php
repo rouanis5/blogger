d7ab3387b181dd8ca582968f0149807be4987f95
rouanis5 <ouanis42@gmail.com>
2022-05-17 17:03:28 +0100

    PHP: + Model class to inherite

---
+++ backend/libraries/Model.php
@@ -0,0 +1,33 @@
+<?php
+class Model
+{
+    protected $connection;
+    private $errors = [];
+
+    public function __construct($db)
+    {
+        $this->connection = $db->connect();
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    public function tryCatchPDO($stmnt, $callBackFunction)
+    {
+        try {
+            if ($stmnt->execute()) {
+                $execute_state = true;
+            } else {
+                $execute_state = false;
+            }
+
+            //the user will chose what function he wants
+            return $callBackFunction($execute_state);
+        } catch (PDOException $error) {
+            $this->errors[] = $error->getMessage();
+        }
+
+    }
+}
