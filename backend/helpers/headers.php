c56b4f52eca065d1d5b0b2d64d438f0d79f7d08c
rouanis5 <ouanis42@gmail.com>
2022-05-19 19:31:58 +0100

    + headers.php

---
+++ backend/helpers/headers.php
@@ -0,0 +1,11 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode((object) ['message' => 'Only POST requests are allowed !']);
+    exit;
+}
