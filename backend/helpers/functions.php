6f02ecb750170336b81a7bb1eaa32f0114dba1c7
rouanis5 <ouanis42@gmail.com>
2022-05-19 11:48:04 +0100

    PHP: + validateDate ()

---
+++ backend/helpers/functions.php
@@ -0,0 +1,8 @@
+<?php
+//the date must be like '2022-12-31'
+function validateDate($date, $format = 'Y-m-d')
+{
+    $d = DateTime::createFromFormat($format, $date);
+    // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
+    return $d && $d->format($format) === $date;
+}
