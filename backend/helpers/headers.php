a5eaf633a3a3a1c4507aa7ac9b38c6af11af61c8
rouanis5 <ouanis42@gmail.com>
2022-05-20 12:08:43 +0100

    headers.php: +return a output object

--- backend/helpers/headers.php
+++ backend/helpers/headers.php
@@ -6,6 +6,9 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    echo json_encode((object) ['message' => 'Only POST requests are allowed !']);
+    require realpath(__DIR__ . '/../helpers/classes/Output.php');
+    $out = new Output();
+    $out->push('Only POST requests are allowed !');
+    echo json_encode($out);
     exit;
 }
