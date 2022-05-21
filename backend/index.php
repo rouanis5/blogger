81ad884b65a785f144b35f9d970e8ebc4d015c6d
rouanis5 <ouanis42@gmail.com>
2022-05-21 14:21:39 +0100

    php: + routes + action post variable

--- backend/index.php
+++ backend/index.php
@@ -1,12 +1,6 @@
 <?php
 // require realpath(__DIR__ . '/router.php');
-require realpath(__DIR__ . '/helpers/classes/Output.php');
-require realpath(__DIR__ . '/controller/post.php');
-// // require realpath(__DIR__ . '/controller/comment.php');
-
-// // $c = new CommentConroller();
-
-echo $_SERVER['REQUEST_URI'];
+require_once realpath(__DIR__ . '/helpers/classes/Output.php');
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     $out = new Output();
     $out->push('Only POST METHOD is accepted !');
@@ -15,17 +9,61 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 $routes = [];
-$p = new PostConroller();
+$action = $_POST['action'] ?? null;
 
-route('/getAll', function () use ($p) {
-    $p->getAll();
-});
+switch ($action) {
+    case 'post':
+        require_once realpath(__DIR__ . '/controller/post.php');
+        $p = new PostConroller();
+        route('/get/all', function () use ($p) {
+            $p->getAll();
+        });
 
-route('/get', function () use ($p) {
-    $p->get();
-});
+        route('/get', function () use ($p) {
+            $p->get();
+        });
 
-route('/404', function () use ($p) {
+        route('/add', function () use ($p) {
+            $p->add();
+        });
+
+        route('/update', function () use ($p) {
+            $p->update();
+        });
+
+        route('/delete', function () use ($p) {
+            $p->delete();
+        });
+
+        break;
+    case 'comment':
+        require_once realpath(__DIR__ . '/controller/comment.php');
+        $c = new CommentConroller();
+        route('/getAll', function () use ($c) {
+            $c->getAll();
+        });
+
+        route('/get', function () use ($c) {
+            $c->get();
+        });
+
+        route('/add', function () use ($c) {
+            $c->add();
+        });
+
+        route('/update', function () use ($c) {
+            $c->update();
+        });
+
+        route('/delete', function () use ($c) {
+            $c->delete();
+        });
+        break;
+    default:
+        break;
+}
+
+route('/404', function () {
     $out = new Output();
     $out->push('URL not valid');
     echo json_encode($out);
