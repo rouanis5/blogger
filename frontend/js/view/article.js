e66992ffe77a3938a1cac066c5823b00e2893cb0
rouanis5 <ouanis42@gmail.com>
2022-05-22 09:53:41 +0100

    + article view

---
+++ frontend/js/view/article.js
@@ -0,0 +1,27 @@
+import View from "./abstractView";
+
+export default class Artcile extends View {
+  constructor(id = "", desc = "", date = "") {
+    super();
+    this.id = id;
+    this.desc = desc;
+    this.date = date;
+  }
+
+  getHtml() {
+    var string = this.desc;
+    var desc = string.substring(0, 150) + "...";
+    return `
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Article ${this.id}</h5>
+          <p class="card-text">${desc}</p>
+          <p class="card-text">
+            <small class="text-muted">Date: ${this.date}</small>
+          </p>
+          <a href="#" class="btn btn-success">Read more</a>
+        </div>
+      </div>
+    `;
+  }
+}
