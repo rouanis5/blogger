638a6c13ec9b8be44d9232e62e611c695c61e932
rouanis5 <ouanis42@gmail.com>
2022-05-23 21:02:58 +0100

    update article card

--- frontend/js/view/articleCard.js
+++ frontend/js/view/articleCard.js
@@ -10,9 +10,16 @@ export default class Article extends View {
 
   getHtml() {
     var string = this.desc;
+    var random = Math.floor(Math.random() * 100);
     var desc = string.substring(0, 150) + "...";
     return `
       <div class="card">
+        <img
+          class="card-img-top"
+          src="https://picsum.photos/200/120?random=${random}"
+          alt="image"
+          height='200px' 
+        />
         <div class="card-body">
           <h5 class="card-title">Article ${this.id}</h5>
           <p class="card-text">${desc}</p>
