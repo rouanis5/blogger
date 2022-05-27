4aa1fe7cd215c73b6d0b37c4e2d6b52336a047a8
rouanis5 <ouanis42@gmail.com>
2022-05-27 19:27:25 +0100

    fix updatePage.js Constroller
    seperate the model from the controller

--- frontend/js/controller/updatePage.js
+++ frontend/js/controller/updatePage.js
@@ -1,9 +1,6 @@
-import API_URL from "../config/env";
+import Post from '../model/post';
 import View from "../helpers/abstractView";
 import AddPage from "../view/insertPage";
-import axios from "axios";
-
-const url = new URL(API_URL + "/get");
 
 export default class updatePage extends View {
   constructor(id) {
@@ -12,22 +9,9 @@ export default class updatePage extends View {
   }
 
   async getHtml() {
-    //this is a model it get the info of an article
-    const params = new URLSearchParams({
-      id: this.id,
-      action: "post",
-    });
-
-    var res;
+    var post = new Post();
+    var res = await post.get(this.id);
     var html = "";
-    try {
-      var req = await axios.post(url, params);
-      res = req.data;
-    } catch (error) {
-      res = { success: false, message: "axios failure" };
-    }
-    
-    // -------------------------------------------------------------------------
 
     if (res.success) {
       var page = new AddPage('Update','updateBtn', res.message.id, res.message.post);
