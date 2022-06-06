693090a07bf0cb0b162b13fba1630aec139b1805
rouanis5 <ouanis42@gmail.com>
2022-06-06 12:15:54 +0100

    + users tables

--- backend/dataBase.sql
+++ backend/dataBase.sql
@@ -28,4 +28,18 @@ CREATE TABLE IF NOT EXISTS `comments` (
   ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Structure de la table `users`
+--
+-- @block
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(2048) NOT NULL,
+  `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
 -- --------------------------------------------------------
