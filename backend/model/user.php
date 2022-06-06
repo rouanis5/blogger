20705a48a211c52c2f820735f49b2441bb33d4c7
rouanis5 <ouanis42@gmail.com>
2022-06-06 17:34:34 +0100

    PHP: + email validation

--- backend/model/user.php
+++ backend/model/user.php
@@ -17,41 +17,78 @@ class User extends Model
   public $modified;
 
   // create new user record
-  function create(){
+  function create($username , $email , $password){
     $out = new Output();
   
-    // insert query
-    $query = "INSERT INTO " . $this->table . " SET
-                `username` = :username,
-                `email` = :email,
-                `password` = :password";
-
-    // prepare the query
-    $stmnt = $this->connection->prepare($query);
-
     // sanitize
-    $this->username = htmlspecialchars(strip_tags($this->username));
-    $this->email = htmlspecialchars(strip_tags($this->email));
-    $this->password = htmlspecialchars(strip_tags($this->password));
+    $username = htmlspecialchars(strip_tags($username));
+    $email = htmlspecialchars(strip_tags($email));
+    $password = htmlspecialchars(strip_tags($password));
 
-    // bind the values
-    $stmnt->bindParam(':username', $this->username);
-    $stmnt->bindParam(':email', $this->email);
+    $validation = $this->isEmailValid($email);
 
-    // hash the password before saving to database
-    $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
-    $stmnt->bindParam(':password', $password_hash);
+    if (! $validation->getSuccess()) {
+      $out->pushArray($validation->getMessages());
+    }
+    
+    if (empty($out->getMessages())){
+      $query = "INSERT INTO " . $this->table . " SET
+                  `username` = :username,
+                  `email` = :email,
+                  `password` = :password";
+      $stmnt = $this->connection->prepare($query);
 
-    // execute the query, also check if query was successful
-    $this->tryCatchPDO($stmnt, function() use ($out , $stmnt){
-      //if the sql didnt runs, the rount will be 0
-      //rowCount() number of rows that were deleted
-      if ($stmnt->rowCount() === 0) {
-        $out->push('Adding new user failed');
-      } else {
-        $out->true();
-      }
-    });
+      $stmnt->bindParam(':username', $username);
+      $stmnt->bindParam(':email', $email);
+
+      // hash the password before saving to database
+      $password_hash = password_hash($password, PASSWORD_BCRYPT);
+      $stmnt->bindParam(':password', $password_hash);
+
+      // execute the query, also check if query was successful
+      $this->tryCatchPDO($stmnt, function() use ($out , $stmnt){
+        //if the sql didnt runs, the rount will be 0
+        //rowCount() number of rows that were deleted
+        if ($stmnt->rowCount() === 0) {
+          $out->push('Adding new user failed');
+        } else {
+          $out->true();
+        }
+      });
+    }
+    return $out;
+  }
+
+  // check if given email exist in the database
+  function isEmailValid($email, $except = null){
+    $out = new Output();
+
+    //it's OK if the except and the email are the same
+    if ($except && $except === $email) {
+      $out->true();
+      return $out;
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $out->push("Email not valid");
+    }else{
+      $query = "SELECT `id` FROM " . $this->table . " WHERE `email` = :email LIMIT 0,1";
+      $stmt = $this->connection->prepare( $query );
+  
+      // sanitize
+      $email=htmlspecialchars(strip_tags($email));
+      $stmt->bindParam(':email', $email);
+  
+      $this->tryCatchPDO($stmt, function() use ($stmt, &$out){
+        // get number of rows
+        $num = $stmt->rowCount();
+        if ($num > 0) {
+          $out->push('Email exists !');
+        }else{
+          $out->true();
+        }
+      });
+    }
 
     return $out;
   }
